28168c58bbc8d129bae98fa86f9758aec5d4f623
Chris Clark <ludachrispeed@gmail.com>
2018-03-14 09:03:53 -0400

    Simplify HTML more

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+<div id="app">
+  <img src="./assets/logo.png">
+  <h1>{{msg}}</h1>
+  <ul>
+    <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+    <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+    <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+  </ul>
+</div>
 </template>
 
 <script>
@@ -17,7 +15,7 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Vue Starter'
+      msg: 'App Component msg'
     }
   }
 }
@@ -25,26 +23,13 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
 }
 
 ul {
   list-style-type: none;
   padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
 }
 
 a {
