3612311c071b288a85561c72959ba87ed61861c4
Chris Clark <ludachrispeed@gmail.com>
2018-03-15 09:15:00 -0400

    Move components to components directory

---
+++ src/components/Main.vue
@@ -0,0 +1,34 @@
+<template>
+<div id="main">
+  <img src="./logo.png">
+  <h1>{{msg}}</h1>
+  <ul>
+    <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+    <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+    <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Main Component msg'
+    }
+  }
+}
+</script>
+
+<style>
+#main {
+  border: 1px solid red;
+  text-align: center;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,26 @@
+<template>
+<div id="sidebar">
+  <h1>{{msg}}</h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Sidebar Component msg'
+    }
+  }
+}
+</script>
+
+<style>
+#sidebar {
+  color: #A122FF;
+}
+
+a {
+  color: #42b983;
+}
+</style>
